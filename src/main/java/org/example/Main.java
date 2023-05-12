6de2c516ba1e5513a11abce64d7575e9e6ccda46
goblin-3 <gielmey007@gmail.com>
2023-05-12 18:10:33 +0200

    added functions to check if amount dubbels is not exceeded.Main is not working yet

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,23 @@
 package org.example;
 
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        Spelverdeling solver = new Spelverdeling(6,5,1,5);
+        Spelverdeling solver = new Spelverdeling();
 
+        Optional<String[][]> solution = solver.solve(6,5,2,5);
+        if (solution.isEmpty()) {
+            System.out.println("Geen oplossing");
+        }
+        else {
+            /*
+            String[][] oplossing = solution.get();
+            for (String[]  rondes: oplossing) {
+                System.out.println(rondes);
+                */
 
+            }
+        }
 
-        System.out.println("Hello world!");
     }
-}
--- src/main/java/org/example/Spelverdeling.java
+++ src/main/java/org/example/Spelverdeling.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public class Spelverdeling {
-    private int ploegen,spellen,dubbels,rondes;
 
+    private ArrayList<Character> TeamList ;
+    private int [][] gamesPlayed;
+    /*
     public Spelverdeling(int ploegen, int spellen, int dubbels, int rondes) {
         this.ploegen = ploegen;
         this.spellen = spellen;
@@ -12,11 +15,55 @@ public class Spelverdeling {
         this.rondes = rondes;
     }
 
-    public Optional<String[][]>spelverdeling(int ploegen, int spellen, int dubbels, int rondes){
+*/
 
 
-return NULL;
+    public Optional<String[][]>solve(int ploegen, int spellen, int dubbels, int rondes){
+
+
+
+        for (int i=0 ; i<ploegen;i++) {
+            for (char ch = 'A'; ch <= 'Z'; ch++) {
+                TeamList.add(ch);
+            }
+        }
+            gamesPlayed = new int[ploegen][ploegen];
+
+        String[][] temporarySolution = new String[rondes][spellen];
+
+        int currentronde = 0;
+
+
+        if (currentronde == rondes){
+            return Optional.of(temporarySolution);
+        }
+
+
+
+
+temporarySolution[0][0] = "A-B";
+        return Optional.empty();
 
 
     }
+
+public void addGame(int team1,int team2){
+        gamesPlayed[team1][team2]++;
+        gamesPlayed[team2][team1]++;
+}
+    public int getGamesPlayed(int team1,int team2){
+        return gamesPlayed[team1][team2];
+    }
+    public boolean isNotOverDublesLimit(int team1,int team2,int dubbels){
+        int amount = getGamesPlayed(team1,team2);
+        if(amount >= dubbels){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+
+
 }
