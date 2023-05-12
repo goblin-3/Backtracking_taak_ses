2c8f3388ee66eb294c7579beb939864b8afd9c4d
goblin-3 <gielmey007@gmail.com>
2023-05-12 11:47:54 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Spelverdeling solver = new Spelverdeling(6,5,1,5);
+
+
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/Spelverdeling.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Optional;
+
+public class Spelverdeling {
+    private int ploegen,spellen,dubbels,rondes;
+
+    public Spelverdeling(int ploegen, int spellen, int dubbels, int rondes) {
+        this.ploegen = ploegen;
+        this.spellen = spellen;
+        this.dubbels = dubbels;
+        this.rondes = rondes;
+    }
+
+    public Optional<String[][]>spelverdeling(int ploegen, int spellen, int dubbels, int rondes){
+
+
+return NULL;
+
+
+    }
+}
