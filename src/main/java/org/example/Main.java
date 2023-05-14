3fa737d45024d8c5c6dee8eee36b72bae0b5607c
Stig Konings <80033817+stigkonings@users.noreply.github.com>
2023-05-14 08:51:42 +0200

    Update Main.java
    backtrack code toegevoegd niet getest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,4 +20,44 @@ public class Main {
             }
         }
 
+    
+    weet niet of dit werkt ------------------------------------------------------------------------------------------
+        
+        private static boolean backtrack(String[][] result, int ploegen, int spelletjes, int dubbels, int rondes, int ronde) {
+        if (ronde == rondes) {
+            return true;
+        }
+
+        for (int spel = 0; spel < spelletjes; spel++) {
+            String[] deelnemers = new String[2];
+
+            for (int i = 0; i < 2; i++) {
+                boolean deelnemerGevonden = false;
+
+                for (int ploeg = 0; ploeg < ploegen && !deelnemerGevonden; ploeg++) {
+                    if (ploegKanSpelen(result, spel, ploeg, ronde, i, dubbels)) {
+                        deelnemers[i] = "" + (char) ('A' + ploeg);
+                        deelnemerGevonden = true;
+                    }
+                }
+
+                if (!deelnemerGevonden) {
+                    return false; 
+                }
+            }
+
+            result[ronde][spel] = deelnemers[0] + "-" + deelnemers[1];
+
+            if (backtrack(result, ploegen, spelletjes, dubbels, rondes, ronde + 1)) {
+                return true; 
+            }
+
+            result[ronde][spel] = null; 
+        }
+
+        return false; 
+    }
+    
+    
+    
     }
