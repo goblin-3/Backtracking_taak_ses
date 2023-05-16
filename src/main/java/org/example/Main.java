3c0fec8da44d7be4aaccc021b2ae868cb5d8805b
goblin-3 <gielmey007@gmail.com>
2023-05-16 23:01:07 +0200

    1 bug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,57 +15,26 @@ public class Main {
             System.out.println("Geen oplossing");
         } else {
             String[][] spelverdeling = solution.get();
+
+            // Print game numbers on the top
+            System.out.print("          ");
+            for (int s = 0; s < spelletjes; s++) {
+                System.out.printf("Spel %d  ", s + 1);
+            }
+            System.out.println();
+
+            // Print the grid with round numbers on the side
             for (int r = 0; r < rondes; r++) {
-                System.out.printf("Ronde %d:\n", r + 1);
+                System.out.printf("Ronde %d:  ", r + 1);
                 for (int s = 0; s < spelletjes; s++) {
-                    System.out.printf("  Spel %d: %s\n", s + 1, spelverdeling[r][s]);
+                    System.out.printf("%-8s  ", spelverdeling[r][s]);
                 }
-            }
+                System.out.println();
             }
         }
-
 
     }
 
-/*
-    
-    weet niet of dit werkt ------------------------------------------------------------------------------------------
-        
-        private static boolean backtrack(String[][] result, int ploegen, int spelletjes, int dubbels, int rondes, int ronde) {
-        if (ronde == rondes) {
-            return true;
-        }
 
-        for (int spel = 0; spel < spelletjes; spel++) {
-            String[] deelnemers = new String[2];
-
-            for (int i = 0; i < 2; i++) {
-                boolean deelnemerGevonden = false;
-
-                for (int ploeg = 0; ploeg < ploegen && !deelnemerGevonden; ploeg++) {
-                    if (ploegKanSpelen(result, spel, ploeg, ronde, i, dubbels)) {
-                        deelnemers[i] = "" + (char) ('A' + ploeg);
-                        deelnemerGevonden = true;
-                    }
-                }
-
-                if (!deelnemerGevonden) {
-                    return false; 
-                }
-            }
-
-            result[ronde][spel] = deelnemers[0] + "-" + deelnemers[1];
-
-            if (backtrack(result, ploegen, spelletjes, dubbels, rondes, ronde + 1)) {
-                return true; 
-            }
-
-            result[ronde][spel] = null; 
-        }
-
-        return false; 
-    }
-    
-    */
-    
+}
 
--- src/main/java/org/example/Spelverdeling.java
+++ src/main/java/org/example/Spelverdeling.java
@@ -12,14 +12,14 @@ public class Spelverdeling {
     private int dubbelsT;
 
 
-
-
     private ArrayList<Character> TeamList = new ArrayList<>();
-    private int [][] gamesPlayed;
-    private int [][] SpelGespeeld;
+    private int[][] gamesPlayed;
+    private int[][] SpelGespeeld;
+    private boolean[][] playedCombinations;
+
     private int currentGame = 0;
-    private int team1=-1;
-    private int team2=-1;
+    private int team1 = -1;
+    private int team2 = -1;
 
 
     public Spelverdeling(int ploegenT, int spellenT, int dubbelsT, int rondesT) {
@@ -29,118 +29,210 @@ public class Spelverdeling {
         this.dubbelsT = dubbelsT;
     }
 
-    public Optional<String[][]>solve(int ploegen, int spellen, int dubbels, int rondes){
-        setupTeamList(ploegen);
-        String[][] temporarySolution = new String[rondes][spellen];
+    public Optional<String[][]> solve(int ploegen, int spellen, int dubbels, int rondes) {
+        List<String> combinations = setupAllOptions(ploegen);
+        String[][] solutionList = new String[rondes][spellen];
         gamesPlayed = new int[ploegen][ploegen];
         SpelGespeeld = new int[ploegen][spellen];
-        List<Integer> availablespellen = new ArrayList<>();
-        for (int i=0;i<spellen;i++){
-            availablespellen.add(i);
-        }
+        playedCombinations = new boolean[ploegen][ploegen];
 
-        return solve(ploegen,0,dubbels,0,temporarySolution,availablespellen);
+        Teamlist();
+        //  int [][] availablespellen= new int[rondes][spellen];
+
+
+        return solve(ploegen, 0, dubbels, 0, combinations, solutionList);
     }
 
-    private Optional<String[][]>solve(int ploegen, int currentspellen, int dubbels, int currentronde, String[][] solution, List<Integer> previousattemps){
+    private Optional<String[][]> solve(int ploegen, int currentSpellen, int dubbels, int currentRonde, List<String> combinations, String[][] solution) {
+        if (currentRonde == rondesT) {
+            if (currentSpellen == spellenT) {
 
-        if (currentronde == rondesT){
-            return Optional.of(solution);
+                return Optional.of(solution);
+
+
+            } else {
+                return solve(ploegen, currentSpellen + 1, dubbels, currentRonde, combinations, solution);
+            }
         }
-        if (rondesT == currentronde+1){
-            if(currentspellen == spellenT){
-                return Optional.empty();
+
+        if (currentSpellen == spellenT) {
+            return solve(ploegen, 0, dubbels, currentRonde + 1, combinations, solution);
+        }
+
+        if (IsAtEndOfSpellen(solution,currentSpellen,currentRonde)){
+            List<String> Recombinations = setupAllOptions(ploegen);
+            return solve(ploegen, 0, dubbels, currentRonde + 1, Recombinations, solution);
+        }
+
+        if(combinations.isEmpty()){
+            //combinations = setupAllOptions(ploegen);
+            return solve(ploegen, currentSpellen + 1, dubbels, currentRonde, setupAllOptions(ploegen), solution);
+        }
+        String combination = combinations.get(0);
+
+
+        // Check if the current combination is valid
+        if (isCombinationValid(combination, currentRonde, currentSpellen, solution)) {
+            // Update the solution with the current combination
+            solution[currentRonde][currentSpellen] = combination;
+
+            // Mark the teams and games as played
+            markTeamsAndGamesAsPlayed(combination, currentRonde, currentSpellen);
+
+            // Recursively solve the subproblem by moving to the next game
+            Optional<String[][]> result = solve(ploegen, currentSpellen + 1, dubbels, currentRonde, combinations, solution);
+
+            // If a valid solution is found, return it
+            if (result.isPresent()) {
+                return result;
             }
 
-
-          int  huidigeRonde = currentronde -1;
-          int huidigspel =currentspellen;
-            return solve(ploegen, huidigspel, dubbels, huidigeRonde, solution,previousattemps);
+            // Backtrack by removing the current combination and unmarking teams/games as played
+            solution[currentRonde][currentSpellen] = null;
+            unmarkTeamsAndGamesAsPlayed(combination, currentRonde, currentSpellen);
         }
-        if (currentGame!= spellenT){
+        if (combinations.isEmpty()) {
             return Optional.empty();
         }
-        int huidigeRonde =currentronde;
-        int huidigSpel = currentspellen;
+        // Recursively solve the subproblem by trying the remaining combinations
+
+
+        combinations.remove(0);
+
+        return solve(ploegen, currentSpellen, dubbels, currentRonde, combinations, solution);
+    }
+
+
+    private boolean isCombinationValid(String combination, int currentRonde, int currentSpellen, String[][] solution) {
+        // Check if the teams in the combination have already played the maximum number of games
+        String[] teams = combination.split(" - ");
+        int teamA = teams[0].charAt(0) - 'A';
+        int teamB = teams[1].charAt(0) - 'A';
+
+        if (gamesPlayed[teamA][teamB] >= dubbelsT || gamesPlayed[teamB][teamA] >= dubbelsT) {
+            return false;
+        }
+
+        // Check if the current game has already been played in a previous round
+        for (int r = 0; r < currentRonde; r++) {
+            if (solution[r][currentSpellen] != null && solution[r][currentSpellen].equals(combination)) {
+                return false;
+            }
+        }
+
+        // Check if any of the teams in the current game have already played the maximum number of games in this round
+        if (SpelGespeeld[teamA][currentSpellen] >= 1 || SpelGespeeld[teamB][currentSpellen] >= 1) {
+            return false;
+        }
+
+        // Check if any of the teams in the current game have played in the current round before
+        for (int s = 0; s < currentSpellen; s++) {
+            if (solution[currentRonde][s] != null) {
+                String[] previousTeams = solution[currentRonde][s].split(" - ");
+                if (previousTeams[0].equals(teams[0]) || previousTeams[0].equals(teams[1]) ||
+                        previousTeams[1].equals(teams[0]) || previousTeams[1].equals(teams[1])) {
+                    return false;
+                }
+            }
+        }
+
+        if (playedCombinations[teamA][teamB] || playedCombinations[teamB][teamA]) {
+            return false;
+        }
+
+
+        return true;
+    }
+
+    private void markTeamsAndGamesAsPlayed(String combination, int currentRonde, int currentSpellen) {
+        String[] teams = combination.split(" - ");
+        int teamA = teams[0].charAt(0) - 'A';
+        int teamB = teams[1].charAt(0) - 'A';
+
+        gamesPlayed[teamA][teamB]++;
+        gamesPlayed[teamB][teamA]++;
+
+        SpelGespeeld[teamA][currentSpellen]++;
+        SpelGespeeld[teamB][currentSpellen]++;
+        playedCombinations[teamA][teamB] = true;
+        playedCombinations[teamB][teamA] = true;
+
+
+    }
+
+
+    private void unmarkTeamsAndGamesAsPlayed(String combination, int currentRonde, int currentSpellen) {
+        String[] teams = combination.split(" - ");
+        int teamA = teams[0].charAt(0) - 'A';
+        int teamB = teams[1].charAt(0) - 'A';
+
+        gamesPlayed[teamA][teamB]--;
+        gamesPlayed[teamB][teamA]--;
+
+        SpelGespeeld[teamA][currentSpellen]--;
+        SpelGespeeld[teamB][currentSpellen]--;
+        playedCombinations[teamA][teamB] = false;
+        playedCombinations[teamB][teamA] = false;
+
+
+
+    }
+
+    public static List<String> setupAllOptions(int numberOfTeams) {
+        List<String> rounds = new ArrayList<>();
+
+        // Controleer of het aantal teams geldig is
+        // Maak een array van letters om teams voor te stellen (A, B, C, ...)
+        char[] teamLetters = new char[numberOfTeams];
+        for (int i = 0; i < numberOfTeams; i++) {
+            teamLetters[i] = (char) ('A' + i);
+        }
+
+        // Genereer de combinaties van rondes
+        for (int i = 0; i < numberOfTeams - 1; i++) {
+            for (int j = i + 1; j < numberOfTeams; j++) {
+                String round = teamLetters[i] + " - " + teamLetters[j];
+                rounds.add(round);
+            }
+        }
+
+        return rounds;
+    }
+
+    public void Teamlist() {
+        TeamList.clear(); // Clear the existing team list
 
         for (int i = 0; i < ploegenT; i++) {
-            for (int j = i + 1; j < ploegenT; j++) {
-                if (getGamesPlayed(i, j) < dubbels && !previousattemps.contains(i * 100 + j)) {
-                    team1 = i;
-                    team2 = j;
-                    break;
+            TeamList.add((char) ('A' + i));
+        }
+    }
+
+
+    public boolean IsAtEndOfSpellen(String[][] solution, int currentSpellen, int currentRonde) {
+        // Check if all teams have played exactly one game in each round
+        int gamesPlayedCount = 0;
+        for (char team : TeamList) {
+            int gamesPlayed = 0;
+            for (int r = 0; r <= currentRonde; r++) {
+                for (int s = 0; s < currentSpellen; s++) {
+                    if (solution[r][s] != null) {
+                        String[] teams = solution[r][s].split(" - ");
+                        if (teams[0].charAt(0) == team || teams[1].charAt(0) == team) {
+                            gamesPlayed++;
+                        }
+                    }
                 }
             }
-            if (team1 != -1 && team2 != -1) {
-                break;
+            if (gamesPlayed == currentRonde + 1) {
+                gamesPlayedCount++;
             }
         }
-
-
-
-
-
-        for (int i = 0; i < spellenT; i++) {
-            if (!getSpelGespeeld(team1, i) && !getSpelGespeeld(team2, i)) {
-                solution[currentronde][currentspellen] = TeamList.get(team1) + "-" + TeamList.get(team2) ;
-                addGame(team1, team2);
-                addSpelGespeeld(team1, i);
-                addSpelGespeeld(team2, i);
-                previousattemps.add(team1 * 100 + team2);
-                Optional<String[][]> result = solve(ploegen, currentspellen + 1, dubbels, huidigeRonde, solution, previousattemps);
-                if (result.isPresent()) {
-                    return result;
-                }
-                removeGame(team1, team2);
-                removeSpelGespeeld(team1, i);
-                removeSpelGespeeld(team2, i);
-                previousattemps.remove(previousattemps.size() - 1);
-            }
-        }
-    return solve(ploegen,0,dubbels,currentronde-1,solution,previousattemps);
-
+        return gamesPlayedCount == TeamList.size();
     }
 
-public void addGame(int team1,int team2){
-        gamesPlayed[team1][team2]++;
-        gamesPlayed[team2][team1]++;
-}
-public void removeGame(int team1,int team2){
-    gamesPlayed[team1][team2]--;
-    gamesPlayed[team2][team1]--;
-}
-    public int getGamesPlayed(int team1,int team2){
-        return gamesPlayed[team1][team2];
-    }
-    public boolean isNotOverDublesLimit(int team1,int team2,int dubbels){
-        int amount = getGamesPlayed(team1,team2);
-        if(amount >= dubbels){
-            return false;
-        }else{
-            return true;
-        }
-    }
-    public void setupTeamList(int ploegen){
-        for (int i=0 ; i<ploegen;i++) {
-            for (char ch = 'A'; ch <= 'Z'; ch++) {
-                TeamList.add(ch);
-            }
-        }
-    }
-public void addSpelGespeeld(int team,int spel){
-        SpelGespeeld[team][spel]++;
-}
-public void removeSpelGespeeld(int team,int spel){
-    SpelGespeeld[team][spel]--;
-}
-public boolean getSpelGespeeld(int team,int spel){
-        if (SpelGespeeld[team][spel]==1){
-            return true;
-        }else{
-            return false;
-        }
+
 }
 
 
 
-}
+
